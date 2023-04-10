b209400b9f62341877d07ad6a4afaad4ca793613
Waby M <gabrielamc.046@gmail.com>
2023-04-10 16:33:38 -0600

    Se modificaron para las verificaciones en contáctenos

--- js/contacto.js
+++ js/contacto.js
@@ -1,3 +1,33 @@
-let textNombre = document.getElementById("textNombre");
-let textEmail = document.getElementById("textEmail");
-let textTelef = document.getElementById("textTelef");
+const Nombre = document.getElementById("textNombre");
+const Email = document.getElementById("textEmail");
+const Telef = document.getElementById("textTelef");
+const Mensaje = document.getElementById("textMensaje");
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let enviar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    parrafo.innerHTML =""
+    if(Nombre.value.length <6){
+        warnings +=`El nombre no es valido <br>`
+        enviar = "true"
+    }
+    if(!regexEmail.test(email.value)){
+        warnings +=`El correo no es valido <br>`
+        enviar = true
+
+    }
+    if(Telef.value.length <10){
+        warnings +=`El número teléfonico no es valido <br>`
+        enviar = true
+    }
+    if (enviar){
+        parrafo.innerHTML = warnings
+    }else {
+        parrafo.innerHTML = "Mensaje enviado"
+    }
+
+})
