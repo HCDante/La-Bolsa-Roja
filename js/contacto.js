59bb5e0366c5fc096124c5e8c97a07f427a93cfb
CJBA <barajasavilacristobaljair@gmail.com>
2023-04-17 23:13:39 -0600

    Se añade el footer con estilo css, se quita ventana secundaria al boton enviar en contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -6,52 +6,52 @@ const form = document.getElementById("form");
 const parrafo = document.getElementById("warnings");
 
 
-form.addEventListener("submit", e=>{
+form.addEventListener("submit", e => {
     e.preventDefault();
     let warnings = "";
     let enviar = false;
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let regexTel = /^(\(\d{3}\)|\d{3})-?\d{3}-?\d{4}$/;
-    parrafo.innerHTML ="";
+    parrafo.innerHTML = "";
     //Se crean variables para aplicarles trim()
     let trimName = nombre.value.trim();
     let trimEmail = email.value.trim();
     let trimTelef = teléfono.value.trim();
     let trimMensaje = mensaje.value.trim();
 
-    if(trimName.length <=2){
-        warnings +=`El nombre no es valido <br>`;
+    if (trimName.length <= 2) {
+        warnings += `El nombre no es valido <br>`;
         enviar = "true";
         nombre.style.border = "solid 0.2rem red";
 
-    }else{
+    } else {
         nombre.style.border = "solid 0.2rem green";
     }
-    if(!regexEmail.test(trimEmail)){
-        warnings +=`El correo no es valido <br>`;
+    if (!regexEmail.test(trimEmail)) {
+        warnings += `El correo no es valido <br>`;
         enviar = true;
         email.style.border = "solid 0.2rem red";
-    }else{
+    } else {
         email.style.border = "solid 0.2rem green";
     }
     //if(teléfono.value.length <10)
-    if(!regexTel.test(trimTelef)){
-        warnings +=`El número teléfonico no es valido <br>`;
+    if (!regexTel.test(trimTelef)) {
+        warnings += `El número teléfonico no es valido <br>`;
         enviar = true;
         teléfono.style.border = "solid 0.2rem red";
-    }else{
+    } else {
         teléfono.style.border = "solid 0.2rem green";
     }
-    if(trimMensaje.length < 15){
+    if (trimMensaje.length < 15) {
         warnings += `Mensaje muy corto<br>`;
         enviar = true;
         mensaje.style.border = "solid 0.2rem red";
-    }else{
+    } else {
         mensaje.style.border = "solid 0.2rem green";
     }
-    if (enviar){
+    if (enviar) {
         parrafo.innerHTML = warnings;
-    }else {
+    } else {
         let ebody = `
     <h1>Nombre: </h1>${trimName}
     <br>
@@ -61,17 +61,15 @@ form.addEventListener("submit", e=>{
     <br>
     <h1>Mensaje: </h1>${trimMensaje}
     `;
-    Email.send({
-        SecureToken : "2beb6908-098a-4ce9-8217-645950d7272e",
-        To : 'javadabbado@gmail.com',
-        From : "javadabbado@gmail.com",
-        Subject : "This is the subject",
-        Body : ebody
-    }).then(
-      message => alert(message)
-    );
-        parrafo.innerHTML = 
-        `<div  class="alert alert-success d-flex align-items-center" role="alert">
+        Email.send({
+            SecureToken: "2beb6908-098a-4ce9-8217-645950d7272e",
+            To: 'javadabbado@gmail.com',
+            From: "javadabbado@gmail.com",
+            Subject: "This is the subject",
+            Body: ebody
+        })
+        parrafo.innerHTML =
+            `<div  class="alert alert-success d-flex align-items-center" role="alert">
             <svg  height="2rem"width="2rem" class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
             <div>
                 Mensaje enviado correctamente.
