eaafb7bb6ddbdd824bf1c5fe256b697605afcf66
Waby M <gabrielamc.046@gmail.com>
2023-04-20 23:16:03 -0600

    Se agregaron modificaciones en js para las alertas

--- js/inventario.js
+++ js/inventario.js
@@ -1,3 +1,84 @@
+const  ID_Producto = document.getElementById("ID");
+const nombre = document.getElementById("name");
+const descripcion = document.getElementById("description");
+const precio = document.getElementById("price");
+const imagen = document.getElementById("selection");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+const btnEnviar = document.getElementById("btn-enviar");
+
+//form.addEventListener("submit", e => { e.preventDefault()});
+
+ console.log("Hola com estas");
+btnEnviar.addEventListener("click", e => {
+    console.log("Si entra a la funcion");
+    e.preventDefault();
+    let warnings = "";
+    let agregar = false;
+    //let regexPrice = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+    let regexPrice = /^\d/;
+    parrafo.innerHTML = "";
+
+    let trimID = ID_Producto.value.trim();
+    let trimName = nombre.value.trim();
+    let trimDescription = descripcion.value.trim();
+    let trimPrice = precio.value.trim();
+    //let trimSelection = imagen.value.trim();
+
+    if (trimID.length <= 1) {
+        warnings += `El código no es válido <br>`;
+        agregar = "true";
+        ID_Producto.style.border = "solid 0.15rem red";
+
+    } else {
+        ID_Producto.style.border = "solid 0.15rem green";
+    }
+    if (trimName.length <= 2) {
+        warnings += `El nombre no es válido <br>`;
+        agregar = "true";
+        nombre.style.border = "solid 0.15rem red";
+
+    } else {
+        nombre.style.border = "solid 0.15rem green";
+    }
+    if (trimDescription.length < 15) {
+        warnings += `Descripción muy corta<br>`;
+        agregar = true;
+        descripcion.style.border = "solid 0.15rem red";
+    } else {
+        descripcion.style.border = "solid 0.15rem green";
+    }
+    if (!regexPrice.test(trimPrice)||trimPrice==0) {
+        warnings += `El precio no es válido <br>`;
+        agregar = true;
+        precio.style.border = "solid 0.15rem red";
+    } else {
+        precio.style.border = "solid 0.15rem green";
+    }
+    // Seccion para validar si un imagen fue subida !!!!PENDIENTE!!!!
+    // if (trimSelection) {
+    //     warnings += `No se ha seleccionado alguna imagen <br>`;
+    //     agregar = "true";
+    //     nombre.style.border = "solid 0.2rem red";
+
+    // } else {
+    //     nombre.style.border = "solid 0.2rem green";
+    // }
+    if (agregar) {
+        parrafo.innerHTML = warnings;
+    } else {
+
+      parrafo.innerHTML =
+      `<div  class="alert alert-success d-flex align-items-center" role="alert">
+      <svg  height="2rem"width="2rem" class="bi flex-shrink-0 me-2" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+      <div>
+          Mensaje enviado correctamente.
+      </div>
+   </div>`;
+    }
+});
+
+
 let inventario;
 let id=0;
 
