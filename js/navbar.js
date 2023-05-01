0a4cc8c170fa2de30f66998678d110c8fb7dbaf0
Waby M <gabrielamc.046@gmail.com>
2023-05-01 13:58:02 -0600

    Se realizaron cambios en el carrousel e imagenes

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,8 @@ document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
 document.getElementById("back_menu").addEventListener("click", ocultar_menu);
 
+let prueba1=document.getElementsByClassName("prueba1");
+
 nav = document.getElementById("nav");
 background_menu = document.getElementById("back_menu");
 
@@ -9,11 +11,13 @@ function mostrar_menu(){
 
     nav.style.right = "0px";
     background_menu.style.display = "block";
+    prueba1.style.display="none";
 }
 
 function ocultar_menu(){
 
     nav.style.right = "-250px";
     background_menu.style.display = "none";
+    prueba1.style.display="flex";
 }
 
