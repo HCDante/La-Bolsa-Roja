a1b45c49449056ef7565e0f9ee3e2d7d2d5e62aa
CJBA <barajasavilacristobaljair@gmail.com>
2023-05-01 14:44:09 -0600

    Se quitan las flechas del carrucel en la página acerca de nosotros mediante el archivo navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,8 @@ document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
 document.getElementById("back_menu").addEventListener("click", ocultar_menu);
 
-let prueba1=document.getElementsByClassName("prueba1");
+let prueba1=document.getElementById("prueba1");
+let prueba2=document.getElementById("prueba2");
 
 nav = document.getElementById("nav");
 background_menu = document.getElementById("back_menu");
@@ -11,13 +12,15 @@ function mostrar_menu(){
 
     nav.style.right = "0px";
     background_menu.style.display = "block";
-    prueba1.style.display="none";
+    prueba1.style="display: none";
+    prueba2.style="display: none";
 }
 
 function ocultar_menu(){
 
     nav.style.right = "-250px";
     background_menu.style.display = "none";
-    prueba1.style.display="flex";
+    prueba1.style="display: flex";
+    prueba2.style="display: flex";
 }
 
